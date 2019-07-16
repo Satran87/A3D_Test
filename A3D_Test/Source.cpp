f00a2941ed8c64d53e4ecf5f92c786dc48bc545c
Satran87 <sergeytrushnikov87@gmail.com>
2019-07-16 03:05:55 +0300

    Test with long long element size

--- A3D_Test/Source.cpp
+++ A3D_Test/Source.cpp
@@ -6,21 +6,21 @@
 
 //#define  NO_ASYNC
 
-//TODO:getline может заменить на fgets?
+const unsigned _110Mb = 115343360;
 
+#include <iostream>
+#include <algorithm>
 #include <cstdio>
 #include <cstdlib>
 #include <fstream>
 #include <string>
-#include <sys\stat.h>
 #include <ctime>
-#include <iostream>
+
 #ifndef NO_ASYNC
 #include <future>
 #endif
 using namespace std;
-std::string MaxLineSize = std::to_string(UINT_MAX);
-unsigned RealMaxLineSize = MaxLineSize.size() + 1;//+1 на /r
+unsigned RealMaxLineSize = 11;//+1 на /r
 class my_array
 {
 public:
@@ -43,7 +43,7 @@ public:
 			size += 100000;
 		}
 	}
-	void add(unsigned value,int index)
+	void add(unsigned value,long index)
 	{
 		arr[index] = value;
 		currentIndex = index;
@@ -62,11 +62,14 @@ private:
 	unsigned *arr;
 };
 
-long filesize(const char* filename)
+bool fileExist(const char* filename)
 {
-	struct stat buff{};
-	stat(filename, &buff);
-	return buff.st_size;
+		if (FILE *file = fopen(filename, "r")) 
+		{
+			fclose(file);
+			return true;
+		}
+			return false;
 }
 
 struct MinHeapNode
@@ -75,7 +78,7 @@ struct MinHeapNode
 	unsigned element;
 
 	// index of the array from which the element is taken 
-	long i;
+	long long i;
 };
 
 // Prototype of a utility function to swap two min heap nodes 
@@ -85,20 +88,20 @@ void swap(MinHeapNode* x, MinHeapNode* y);
 class MinHeap
 {
 	MinHeapNode* harr; // pointer to array of elements in heap 
-	long heap_size;	 // size of min heap 
+	long long heap_size;	 // size of min heap 
 
 public:
 	// Constructor: creates a min heap of given size 
-	MinHeap(MinHeapNode a[], long size);
+	MinHeap(MinHeapNode a[], long long size);
 
 	// to heapify a subtree with root at given index 
-	void MinHeapify(long);
+	void MinHeapify(long long);
 
 	// to get index of left child of node at index i 
-	long left(long i) { return (2 * i + 1); }
+	long long left(long long i) { return (2 * i + 1); }
 
 	// to get index of right child of node at index i 
-	long right(long i) { return (2 * i + 2); }
+	long long right(long long i) { return (2 * i + 2); }
 
 	// to get the root 
 	MinHeapNode getMin() { return harr[0]; }
@@ -114,11 +117,11 @@ public:
 
 // Constructor: Builds a heap from a given array a[] 
 // of given size 
-MinHeap::MinHeap(MinHeapNode a[], long size)
+MinHeap::MinHeap(MinHeapNode a[], long long size)
 {
 	heap_size = size;
 	harr = a; // store address of array 
-	long i = (heap_size - 1) / 2;
+	long long i = (heap_size - 1) / 2;
 	while (i >= 0)
 	{
 		MinHeapify(i);
@@ -129,11 +132,11 @@ MinHeap::MinHeap(MinHeapNode a[], long size)
 // A recursive method to heapify a subtree with root 
 // at given index. This method assumes that the 
 // subtrees are already heapified 
-void MinHeap::MinHeapify(long i)
+void MinHeap::MinHeapify(long long i)
 {
-	long l = left(i);
-	long r = right(i);
-	long smallest = i;
+	long long l = left(i);
+	long long r = right(i);
+	long long smallest = i;
 	if (l < heap_size && harr[l].element < harr[i].element)
 		smallest = l;
 	if (r < heap_size && harr[r].element < harr[smallest].element)
@@ -218,14 +221,12 @@ void mergeSort(unsigned arr[], long l, long r)
 
 // Merges k sorted files. Names of files are assumed 
 // to be 1, 2, 3, ... k 
-void mergeFiles(char *output_file, unsigned k)
+void mergeFiles(char *output_file, const long long k)
 {
 	auto in=new std::fstream[k];
-	for (unsigned i = 0; i < k; ++i)
-	{
+	for (long long i = 0; i < k; ++i)
 		// Open output files in read mode. 
 		in[i] = std::fstream(to_string(i), std::ios::binary | istream::in);
-	}
 
 	// FINAL OUTPUT FILE 
 	
@@ -233,19 +234,16 @@ void mergeFiles(char *output_file, unsigned k)
 
 	// Create a min heap with k heap nodes. Every heap node 
 	// has first element of scratch output file 
-	auto harr = new MinHeapNode[k];
-	unsigned i;
+	const auto harr = new MinHeapNode[k];
+	long long i;
 	std::string tmpStr;
 	tmpStr.reserve(RealMaxLineSize);
 	for (i = 0; i < k; ++i)
 	{
 		// break if no output file is empty and 
 		// index i will be no. of input files 
-		//in[i] >> buff;
 		if (std::getline(in[i], tmpStr))
-		{
 			harr[i].element = std::stoul(tmpStr, nullptr, 10);
-		}
 		else
 			break;
 
@@ -253,7 +251,7 @@ void mergeFiles(char *output_file, unsigned k)
 	}
 	MinHeap hp(harr, i); // Create the heap 
 
-	unsigned count = 0;
+	long long count = 0;
 
 	// Now one by one get the minimum element from min 
 	// heap and replace it with next element. 
@@ -262,7 +260,7 @@ void mergeFiles(char *output_file, unsigned k)
 	{
 		// Get the minimum element and store it in output file 
 		MinHeapNode root = hp.getMin();
-		out << root.element<<"\r\n";
+		out << root.element<< "\n\r";
 
 		// Find the next element that will replace current 
 		// root of heap. The next element belongs to same 
@@ -271,7 +269,7 @@ void mergeFiles(char *output_file, unsigned k)
 			root.element = std::stoul(tmpStr, nullptr, 10);
 		else
 		{
-			root.element = UINT_MAX;
+			root.element = 0xffffffff;//UINT_MAX
 			++count;
 		}
 
@@ -280,7 +278,7 @@ void mergeFiles(char *output_file, unsigned k)
 	}
 
 	// close input and output files 
-	for (unsigned i1 = 0; i1 < k; ++i1)
+	for (long long i1 = 0; i1 < k; ++i1)
 	{
 		in[i1].close();
 	}
@@ -288,49 +286,25 @@ void mergeFiles(char *output_file, unsigned k)
 	delete[]harr;
 	out.close();
 }
-void CreateSortChunkFile(basic_fstream<char>* out,  unsigned* arr, int next_output_file, int i)
+void CreateSortChunkFile( unsigned* arr, long long next_output_file, long i)
 {
-#ifdef NO_ASYNC
+	auto out =std::fstream(std::to_string(next_output_file), std::ios::binary | istream::out);
 	mergeSort(arr, 0, i - 1);
-
-	// write the records to the appropriate scratch output file 
-	// can't assume that the loop runs to run_size 
-	// since the last run's length may be less than run_size 
 	for (int j = 0; j < i; ++j)
-		out[next_output_file] << arr[j]<<"\r\n";
-#else
-	
-	mergeSort(arr, 0, i - 1);
-
-	// write the records to the appropriate scratch output file 
-	// can't assume that the loop runs to run_size 
-	// since the last run's length may be less than run_size 
-	for (int j = 0; j < i; ++j)
-		out[next_output_file] << arr[j] << "\r\n";
+		out << arr[j] << "\n\r";
+#ifndef NO_ASYNC
 	delete[] arr;
 #endif
+	out.close();
 }
 
 // Using a merge-sort algorithm, create the initial runs 
 // and divide them evenly among the output files 
-void createInitialRuns(char *input_file, int run_size,
-	unsigned num_ways)
+long long createInitialRuns(char *input_file, int run_size)
 {
 	// For big input file 
 	auto in = std::fstream(input_file, std::ios::binary | istream::in);
 
-	// output scratch files 
-	auto out=new std::fstream[num_ways];
-	char fileName[5];
-	for (unsigned i = 0; i < num_ways; ++i)
-	{
-		// convert i to string 
-		snprintf(fileName, sizeof(fileName), "%d", i);
-
-		// Open output files in write mode. 
-		
-		out[i] = std::fstream(fileName, std::ios::binary | istream::out);
-	}
 
 	// allocate a dynamic array large enough 
 	// to accommodate runs of size run_size 
@@ -338,18 +312,14 @@ void createInitialRuns(char *input_file, int run_size,
 	my_array arrMy;
 
 	bool more_input = true;
-	int next_output_file = 0;
+	long long next_output_file = 0;
 
-	unsigned i;
+	long i;
 	std::string line;
 #ifndef NO_ASYNC
-	auto MaxThread=std::thread::hardware_concurrency();
+	unsigned MaxThread = 4;// std::thread::hardware_concurrency();
 	if (MaxThread == 0)
 		MaxThread = 1;
-	if(num_ways<MaxThread)
-	{
-		MaxThread = num_ways;
-	}
 	auto myThreads = new future<void>[MaxThread];
 #endif
 	std::string tmpStr;
@@ -377,12 +347,10 @@ void createInitialRuns(char *input_file, int run_size,
 		run_sizeTmp = run_size;
 		// sort array using merge sort 
 #ifdef NO_ASYNC
-		CreateSortChunkFile(out, arrMy.GetRaw(), next_output_file, i);
+		CreateSortChunkFile(arrMy.GetRaw(), next_output_file, i);
 #else
 		auto arrTmp = new unsigned[i];
 		std::copy_n(arrMy.get_raw(), i, arrTmp);
-		/*for (int counter = 0;counter < i;++counter)
-			arrTmp[counter] = arrMy.GetRaw()[counter];*/
 		unsigned myCounter = 0;
 		while (true)
 		{
@@ -405,7 +373,7 @@ void createInitialRuns(char *input_file, int run_size,
 		}
 		if (ttt < myCounter)
 			ttt = myCounter;
-		myThreads[myCounter] = std::async(std::launch::async, CreateSortChunkFile, out, arrTmp, next_output_file, i);
+		myThreads[myCounter] = std::async(std::launch::async, CreateSortChunkFile, arrTmp, next_output_file, i);
 #endif
 		++next_output_file;
 	}
@@ -417,44 +385,38 @@ void createInitialRuns(char *input_file, int run_size,
 			myThreads[myCounter1].wait();
 	}
 #endif
-	// close input and output files 
-	for (i = 0; i < num_ways; ++i)
-		out[i].close();
 	in.close();
-	delete[] out;
+	delete[] myThreads;
+	return next_output_file;
 }
 
 // For sorting data stored on disk 
-void externalSort(char* input_file, char *output_file,
-	unsigned num_ways, int run_size)
+void externalSort(char* input_file, char *output_file, int run_size)
 {
 	// read the input file, create the initial runs, 
 	// and assign the runs to the scratch output files 
-	createInitialRuns(input_file, run_size, num_ways);
+	const auto totalFiles=createInitialRuns(input_file, run_size);
 
 	// Merge the runs using the K-way merging 
-	mergeFiles(output_file, num_ways);
+	mergeFiles(output_file, totalFiles);
 }
 
 // Driver program to test above 
 int main(int argc, char* argv[])
 {
-	// The size of each partition 
 	clock_t t1 = clock();
-
-	
-	constexpr auto MinStringSize = 2;//ћинимальна€ длина строки
-	constexpr int run_size = 20*1024*1024;
-	constexpr int maxLinesInRun_Size = run_size/MinStringSize;
-	char input_file[] = "D:\\Sample\\input";
-	char output_file[] = "D:\\Sample\\output";
-	const auto myFileSize=filesize(input_file);
-	if (myFileSize <= 0)
-		return EXIT_FAILURE;
-	int num_ways = myFileSize / run_size;
-	if (myFileSize % run_size !=0)
-		num_ways += 1;
-	externalSort(input_file, output_file, num_ways,
+	constexpr int run_size = 30*1024*1024;
+	/*char input_file[] = "D:\\Sample\\input";
+	char output_file[] = "D:\\Sample\\output";*/
+	char input_file[] = "F:\\Sample\\input";
+	char output_file[] = "F:\\Sample\\output";
+	if (!fileExist(input_file))
+	{
+		auto tmp=std::fstream(output_file, std::ios::binary | istream::out);
+		tmp.close();
+		return EXIT_SUCCESS;
+	}
+	externalSort(input_file, output_file,
 		run_size);
 
 	clock_t t2 = clock();
